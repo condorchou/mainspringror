359c9bd72d7364d37340049e50ea230d138d706b
Homan Chou <homanchou@Homan-Chous-MacBook-Pro-2.local>
2011-03-08 21:26:41 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mindspring::Application.config.secret_token = '3ba32755f7e4abf3f297d402177b7e01eedb63dfdb78984ac59eae2f80c33df022fdf209bc3b0b5040188210fec219f1d4d7ce4e8cfd0965c319f3368aa1f701'
