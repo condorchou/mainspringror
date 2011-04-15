9be074387a563bae89203c05bb5cea3bc3c87d83
Homan Chou <homanchou@Homan-Chous-MacBook-Pro-2.local>
2011-04-15 16:02:19 -0700

    add p3p policy application controller and tested

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :add_compact_policy
+
+  private
+
+  def add_compact_policy
+    # headers["P3P"]='CP="NOI DSP COR NID ADM DEV PSA OUR IND UNI PUR COM NAV INT STA"'
+    # headers["P3P"]='CP="NID DSP ALL COR"'
+    # headers["P3P"]='CP="NOI DSP COR CURa OUR NOR"'
+    headers["P3P"]='CP="NON DSP IVDa OUR IND COM NAV INT DEM"'
+  end
+
 end
