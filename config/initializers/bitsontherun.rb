4fa0dfc774a0098a3e89e9a10b0449d4bc76cdd0
Homan Chou <homanchou@gmail.com>
2012-01-22 19:03:12 -0800

    modify bitsontherun config to use heroku environment vars in production

--- config/initializers/bitsontherun.rb
+++ config/initializers/bitsontherun.rb
@@ -1,6 +1,10 @@
 # Bitsonthrun configuration
-botr_config = YAML::load(File.open('config/bits_on_the_run.yml'))[Rails.env.to_s]
+if Rails.env.to_s == "production"
+  BitsOnTheRun.key = ENV['BOTR_KEY']
+  BitsOnTheRun.secret = ENV['BOTR_SECRET']
+else
+  botr_config = YAML::load(File.open('config/bits_on_the_run.yml'))[Rails.env.to_s]
 
-BitsOnTheRun.key = botr_config['key']
-BitsOnTheRun.secret = botr_config['secret']
-
+  BitsOnTheRun.key = botr_config['key']
+  BitsOnTheRun.secret = botr_config['secret']
+end
