91b46df3d92760cca6ba0248634ba2ec89ec1c28
condorchou <condor.chou@gmail.com>
2012-02-14 21:46:05 -0800

    Fixed date formate from ISO time to human readable format.  javascript sucks at doing this.

--- public/js/mainspring.js
+++ public/js/mainspring.js
@@ -132,6 +132,28 @@
 			});
 		},
 
+		formatDate: function(data){
+			 var dt = new Date(data);
+			var month=new Array(12);
+			month[0]="Jan";
+			month[1]="Feb";
+			month[2]="Mar";
+			month[3]="Apr";
+			month[4]="May";
+			month[5]="Jun";
+			month[6]="Jul";
+			month[7]="Aug";
+			month[8]="Sep";
+			month[9]="Oct";
+			month[10]="Nov";
+			month[11]="Dec";
+			// Get the month, day, and year.
+			var s='';
+			s += month[dt.getMonth()] + " ";
+			s += dt.getDate() + " ";
+			s += dt.getFullYear();
+			return s;	
+		},
 		renderNewComments: function(data){
 			var acomment = {username: this.opts.name,created_at:data.created_at,body:data.body};
 			var data = {comment:acomment};
